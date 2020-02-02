e0f649f2769e522c113332cb1a163b0bd9cf6c27
Lucas Santos <lucas@sugestly.com>
2020-02-02 04:52:17 -0300

    V1.1.0

--- dist/subscribeUtils.d.ts
+++ dist/subscribeUtils.d.ts
@@ -1,3 +1,4 @@
 import { anyFunction } from '@lucasols/utils/typings';
 import { State, EqualityFn } from '.';
 export declare function getIfKeysChange<T extends State>(prev: T, current: T): <K extends keyof T>(keys: Pick<T, K> | K[], callback: anyFunction, checkDeepEquality?: boolean, deepEqualityFn?: EqualityFn<Pick<T, K>>) => void;
+export declare function getIfSelectorChange<T extends State>(prev: T, current: T): <S extends (state: T) => any>(selector: S | [S, ReturnType<S>], callback: anyFunction, checkDeepEquality?: boolean, deepEqualityFn?: EqualityFn<ReturnType<S>>) => void;
--- dist/subscribeUtils.js
+++ dist/subscribeUtils.js
@@ -25,3 +25,22 @@ function getIfKeysChange(prev, current) {
     };
 }
 exports.getIfKeysChange = getIfKeysChange;
+function getIfSelectorChange(prev, current) {
+    return (selector, callback, checkDeepEquality = false, deepEqualityFn = fast_deep_equal_1.default) => {
+        const areEqual = checkDeepEquality ? deepEqualityFn : shallowEqual_1.shallowEqual;
+        const verifyIfChangesTo = Array.isArray(selector);
+        const selectorFn = verifyIfChangesTo
+            ? selector[0]
+            : selector;
+        const currentSelection = selectorFn(current);
+        if (!areEqual(selectorFn(prev), currentSelection)) {
+            if (!verifyIfChangesTo) {
+                callback();
+            }
+            else if (areEqual(currentSelection, selector[1])) {
+                callback();
+            }
+        }
+    };
+}
+exports.getIfSelectorChange = getIfSelectorChange;
