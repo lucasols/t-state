92b9971c74b0b9ec709d35112ca485c16d7d262e
Lucas Santos <lucas@sugestly.com>
2020-02-03 15:00:56 -0300

    v2

--- dist/subscribeUtils.js
+++ dist/subscribeUtils.js
@@ -1,14 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const shallowEqual_1 = require("@lucasols/utils/shallowEqual");
 const pick_1 = require("@lucasols/utils/pick");
-const fast_deep_equal_1 = __importDefault(require("fast-deep-equal"));
 function getIfKeysChange(prev, current) {
-    return (keys, callback, checkDeepEquality = false, deepEqualityFn = fast_deep_equal_1.default) => {
-        const areEqual = checkDeepEquality ? deepEqualityFn : shallowEqual_1.shallowEqual;
+    return (keys, callback, areEqual = shallowEqual_1.shallowEqual) => {
         const verifyIfChangesOnly = Array.isArray(keys);
         const changeToObjKeys = (verifyIfChangesOnly
             ? keys
@@ -26,8 +21,7 @@ function getIfKeysChange(prev, current) {
 }
 exports.getIfKeysChange = getIfKeysChange;
 function getIfSelectorChange(prev, current) {
-    return (selector, callback, checkDeepEquality = false, deepEqualityFn = fast_deep_equal_1.default) => {
-        const areEqual = checkDeepEquality ? deepEqualityFn : shallowEqual_1.shallowEqual;
+    return (selector, callback, areEqual = shallowEqual_1.shallowEqual) => {
         const verifyIfChangesTo = Array.isArray(selector);
         const selectorFn = verifyIfChangesTo
             ? selector[0]
