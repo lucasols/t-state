b9b140ebcb74b2afae6fd35c93056f20777e2267
Lucas Santos <lucas@sugestly.com>
2020-10-22 20:31:51 -0300

    publish v4.2.0

--- src/useCreateStore.ts
+++ src/useCreateStore.ts
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import Store, { Reducers, ReducersPayloads, State, StoreProps } from '.';
 
 export function useCreateStore<
