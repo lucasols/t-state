c85ac8b92d9669988f1ce86f097766cf1d6fe2ca
lucaslos <lucas@sugestly.com>
2023-01-06 19:12:26 -0300

    fix bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: [
-    'src/main.ts',
-    'src/subscribeUtils.ts',
-    'src/useCreateStore.ts',
-    'src/devTools.ts',
-    'src/utils.ts',
-    'src/shallowEqual.ts',
-  ],
+  entry: ['src/main.ts'],
   clean: true,
   format: ['cjs', 'esm'],
   esbuildOptions(options) {
